a0ea5feb420828526db4111fe763a56eca4ea59e
Julia <kuchina@gmail.com>
2017-06-30 14:37:45 +1000

    header and footer complited

--- app/assets/scripts/modules/Portfolio.js
+++ app/assets/scripts/modules/Portfolio.js
@@ -8,10 +8,10 @@ class Portfolio {
 
   insertPortfolio(){
     projects.map(function(item){
-      $("#portfolio > div").append(`<div class="row__small-4 row__medium-4">
+      $("#projects > div").append(`<div class="row__small-4 row__medium-4 row--b-margin-until-medium">
           <a href=${item.link} target="blank">
             <div class="thumbnail">
-              <img src=${item.image +'&raw=1'} alt="${item.title}" class="" />
+              <img src=${item.image} alt="${item.title}" class="" />
               <div class="thumbnail__text">
                 <h3>${item.title}</h3>
 <p class="font-courier thumbnail__text--smaller">${item.info}</p>
@@ -19,11 +19,8 @@ class Portfolio {
           </div>
           </a>
         </div>`);
-      
     });
-    
   }
-
 }
 
 export default Portfolio;
